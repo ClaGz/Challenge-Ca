c813b2e12051f49d023e9ee873eb1b89936b41e8
claudiogivisiez <audio.givisiez@gmail.com>
2021-04-28 03:00:14 -0300

    [feat-receiving-address]  - WIP. Recebe o body parseado;

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+app.use(express.json());
 
 const index = require('./routes/index');
 const localizationRoute = require('./routes/localizationRoute');
--- src/controllers/localizationController.js
+++ src/controllers/localizationController.js
@@ -1,3 +1,4 @@
+//TODO: Fazer as validações do que chega no request com middlewares
 exports.post = (req, res, next) => {
     res.status(201).send('Requisição recebida com sucesso!');
 };
