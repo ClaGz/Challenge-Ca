77113801244030eac5c7491f775e9e388d808f31
Claudio Givisiez <claudio.givisiez@bemobi.com.br>
2021-05-19 23:43:47 -0300

    [teste]  - teste

--- server.js
+++ server.js
@@ -3,14 +3,7 @@ const port = normalizaPort(process.env.PORT || '3000');
 
 function normalizaPort(val) {
   const port = parseInt(val, 10);
-
-  if (isNaN(port)) {
-    return val;
-  }
-  if (port >= 0) {
-    return port;
-  }
-  return false;
+  return port
 }
 
 app.listen(port, function () {
